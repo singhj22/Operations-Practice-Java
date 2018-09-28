42256fae71993eb8a273830c261abf65113d8033
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-09-28 12:27:36 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,28 @@
+
+public class OperationsTest
+{
+
+	public static void main(String[] args) 
+	{
+		int number; //Declaration Statement
+		number= 20; //Assignment Statement -> initialization
+		
+		number+=5; //number=number+5
+		System.out.println(number);
+		
+		number/=2;
+		System.out.println(number);
+		
+		number-=6;
+		System.out.println(number);
+		
+		number*=4;
+		System.out.println(number);
+		
+		number%=2;
+		System.out.println(number);
+		
+		
+	}
+
+}
